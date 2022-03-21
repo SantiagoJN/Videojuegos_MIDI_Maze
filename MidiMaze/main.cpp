a2a0f64a4297bb7fbc2e51a89fe37d82c0a33afc
Santiago Jiménez <62775651+SantiagoJN@users.noreply.github.com>
2022-03-21 20:11:56 +0100

    Prueba cambio

--- MidiMaze/main.cpp
+++ MidiMaze/main.cpp
@@ -1,12 +1,17 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
 
 int main(void)
 {
+    cout << "Funciona!" << endl;
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
